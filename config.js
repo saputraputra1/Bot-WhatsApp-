813e14c88303059d7b171da2005189cab37924f5
saputraputra1 <bungadewi12345@gmail.com>
2025-07-12 00:53:54 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+echo '{
+  "prefix": "!",
+  "adminNumbers": ["62895335107865"],
+  "bannedWords": ["kata1", "kata2"],
+  "apiKeys": {
+    "youtube": "API_KEY",
+    "instagram": "API_KEY"
+  }
+}' > config.json
